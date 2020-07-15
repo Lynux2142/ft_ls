bb4a48e6b68a3e5f41af07786e5d17a6537c1c48
Lucas Guillerot lguiller <lguiller@student.42.fr>
2020-07-15 15:56:02 +0200

    ajout du flag -t pour trier dans l'ordre de modification des fichiers

--- src/sort_file.c
+++ src/sort_file.c
@@ -6,18 +6,29 @@
 /*   By: lguiller <lguiller@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/30 15:16:11 by lguiller          #+#    #+#             */
-/*   Updated: 2020/07/09 15:36:11 by lguiller         ###   ########.fr       */
+/*   Updated: 2020/07/15 15:54:35 by lguiller         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void	sort_file_list(int *flag, t_list *file_list)
+static int	switch_test(int *flag, t_file *a, t_file *b)
+{
+	int cmp;
+
+	cmp = 0;
+	if (flag[4])
+		cmp = b->stat->st_mtime - a->stat->st_mtime;
+	if (cmp == 0 || !flag[4])
+		cmp = ft_strcmp(a->name, b->name);
+	return (flag[3] ? cmp > 0 : cmp < 0);
+}
+
+void		sort_file_list(int *flag, t_list *file_list)
 {
 	t_list	*i;
 	t_list	*j;
 	void	*tmp;
-	int		cmp;
 
 	i = file_list;
 	while (i != NULL)
@@ -25,9 +36,7 @@ void	sort_file_list(int *flag, t_list *file_list)
 		j = file_list;
 		while (j != NULL)
 		{
-			cmp = ft_strcmp(((t_file*)i->content)->name,
-				((t_file*)j->content)->name);
-			if (flag[3] ? cmp > 0 : cmp < 0)
+			if (switch_test(flag, (t_file*)i->content, (t_file*)j->content))
 			{
 				tmp = i->content;
 				i->content = j->content;
